4cc630374e19c35902d020ceeee96ca17f88f037
simransagar093 <84291083+simransagar093@users.noreply.github.com>
2021-07-09 21:02:49 +0530

    Add files via upload

---
+++ DAY-14/tha-14.js
@@ -0,0 +1,22 @@
+function func1(){
+     document.querySelector("button").style.color="green";
+  document.querySelector("button").innerHTML="thanks for clicking";
+
+}
+function func2(){
+  alert("You pressed a key inside the input field");
+}
+function func3(){
+  var x=document.getElementById("no");
+  x.value="";
+
+}
+function func5(){
+  document.getElementById("button2").style.color="green";
+  var a=document.getElementById("button2");
+  a.innerHTML="keep clicking";
+}
+function func4(){
+  document.getElementById("button2").style.color="blue";
+  document.getElementById("button2").innerHTML="have a nice day";
+}
