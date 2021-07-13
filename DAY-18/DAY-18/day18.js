3e177de2b88869ba6f9e60f3afdc40c2dd7cca98
simransagar093 <84291083+simransagar093@users.noreply.github.com>
2021-07-13 17:32:40 +0530

    Add files via upload

---
+++ DAY-18/DAY-18/day18.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chess from './Chess';
+
+
+function Day18(){
+  return(
+  <div>
+    <Chess />
+  </div>);
+}
+export default Day18;
