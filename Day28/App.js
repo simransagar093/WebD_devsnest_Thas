72e1fd3240d0f5842fc094701033ebe3430f0960
simransagar093 <84291083+simransagar093@users.noreply.github.com>
2021-09-16 12:05:39 -0700

    Add files via upload

---
+++ Day28/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css';
+// import Card from "./components/card"
+import day19 from "./components/card"
+// const ek = <h1>hello guyss</h1>
+
+function App() {
+
+  
+  return (
+    <div className="align">
+      <day19 title="Title 1" />
+      <day19 title="Title 2"/>
+      <day19 title="Title 3"/>
+      <day19 title="Title 4"/>
+      </div>
+
+  );
+}
+
+export default App;
